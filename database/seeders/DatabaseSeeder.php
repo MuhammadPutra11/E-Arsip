5abfbe4c922b48cff24077e1f38d8dd6ff150d61
Muhammad Teo Saputra <putrateo012@gmail.com>
2024-07-31 07:01:13 +0800

    final 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,90 +29,97 @@ class DatabaseSeeder extends Seeder
 
         User::create([
             'id' => '2',
-            'username' => 'putra',
-            'password' => 'putra',
-            'no_hp' => '085390511405',
+            'username' => 'jali',
+            'password' => 'jali',
+            'no_hp' => '08539324305',
         ]);
 
-        Dokumen::create([
-            'id' => '1',
-            'user_id' => '1',
-            'nama_dokumen' => 'Lakip',
-            'tanggal_dokumen' => '31 mei',
-            'pengedit_dokumen' => 'putra',
-            'status_id' => '1',
-            'catatan' => 'ea ea ea ea',
-            'file_dokumen' => 'asldas90d8uyap9a'
+        User::create([
+            'id' => '3',
+            'username' => 'rehan',
+            'password' => 'rehan',
+            'no_hp' => '546095435',
         ]);
 
-        Dokumen::create([
-            'id' => '2',
-            'user_id' => '2',
-            'nama_dokumen' => 'Renstra',
-            'tanggal_dokumen' => '31 mei',
-            'pengedit_dokumen' => 'putra2',
-            'status_id' => '2',
-            'catatan' => 'eae eae eae eae',
-            'file_dokumen' => 'bvsd34d8uyap9a'
-        ]);
+        // Dokumen::create([
+        //     'id' => '1',
+        //     'user_id' => '1',
+        //     'nama_dokumen' => 'Lakip',
+        //     'tanggal_dokumen' => '31 mei',
+        //     'pengedit_dokumen' => 'putra',
+        //     'status_id' => '1',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_dokumen' => 'asldas90d8uyap9a'
+        // ]);
 
-        Foto::create([
-            'id' => '1',
-            'user_id' => '1',
-            'nama_foto' => 'dokumentasi',
-            'tanggal_foto' => '31 januari',
-            'catatan' => 'ea ea ea ea',
-            'file_foto' => 'vbnas90d8uyap9a'
-        ]);
+        // Dokumen::create([
+        //     'id' => '2',
+        //     'user_id' => '2',
+        //     'nama_dokumen' => 'Renstra',
+        //     'tanggal_dokumen' => '31 mei',
+        //     'pengedit_dokumen' => 'putra2',
+        //     'status_id' => '2',
+        //     'catatan' => 'eae eae eae eae',
+        //     'file_dokumen' => 'bvsd34d8uyap9a'
+        // ]);
 
-        Foto::create([
-            'id' => '2',
-            'user_id' => '2',
-            'nama_foto' => 'Kegiatan asd',
-            'tanggal_foto' => '23 mei',
-            'catatan' => 'ea ea ea ea',
-            'file_foto' => 'asldas90d8uyap9a'
-        ]);
+        // Foto::create([
+        //     'id' => '1',
+        //     'user_id' => '1',
+        //     'nama_foto' => 'dokumentasi',
+        //     'tanggal_foto' => '31 januari',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_foto' => 'vbnas90d8uyap9a'
+        // ]);
 
-        SuratKeluar::create([
-            'id' => '1',
-            'user_id' => '1',
-            'nama_suratkeluar' => 'surat bapeda',
-            'tanggal_suratkeluar' => '31 januari',
-            'pengirim' => 'putra',
-            'catatan' => 'ea ea ea ea',
-            'file_suratkeluar' => 'vbnas90d8uyap9a'
-        ]);
+        // Foto::create([
+        //     'id' => '2',
+        //     'user_id' => '2',
+        //     'nama_foto' => 'Kegiatan asd',
+        //     'tanggal_foto' => '23 mei',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_foto' => 'asldas90d8uyap9a'
+        // ]);
 
-        SuratKeluar::create([
-            'id' => '2',
-            'user_id' => '2',
-            'nama_suratkeluar' => 'surat rapat',
-            'tanggal_suratkeluar' => '31 januari',
-            'pengirim' => 'putra',
-            'catatan' => 'ea ea ea ea',
-            'file_suratkeluar' => 'asdad8uyap9a'
-        ]);
+        // SuratKeluar::create([
+        //     'id' => '1',
+        //     'user_id' => '1',
+        //     'nama_suratkeluar' => 'surat bapeda',
+        //     'tanggal_suratkeluar' => '31 januari',
+        //     'pengirim' => 'putra',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_suratkeluar' => 'vbnas90d8uyap9a'
+        // ]);
 
-        SuratMasuk::create([
-            'id' => '1',
-            'user_id' => '1',
-            'nama_suratmasuk' => 'sur324 bapeda',
-            'tanggal_suratmasuk' => '31 januari',
-            'pengirim' => 'putra',
-            'catatan' => 'ea ea ea ea',
-            'file_suratmasuk' => 'vbnas90d8uyap9a'
-        ]);
+        // SuratKeluar::create([
+        //     'id' => '2',
+        //     'user_id' => '2',
+        //     'nama_suratkeluar' => 'surat rapat',
+        //     'tanggal_suratkeluar' => '31 januari',
+        //     'pengirim' => 'putra',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_suratkeluar' => 'asdad8uyap9a'
+        // ]);
 
-        SuratMasuk::create([
-            'id' => '2',
-            'user_id' => '2',
-            'nama_suratmasuk' => '879324 bapeda',
-            'tanggal_suratmasuk' => '31 januari',
-            'pengirim' => 'putra',
-            'catatan' => 'ea ea ea ea',
-            'file_suratmasuk' => 'vbnas90d8uyap9a'
-        ]);
+        // SuratMasuk::create([
+        //     'id' => '1',
+        //     'user_id' => '1',
+        //     'nama_suratmasuk' => 'sur324 bapeda',
+        //     'tanggal_suratmasuk' => '31 januari',
+        //     'pengirim' => 'putra',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_suratmasuk' => 'vbnas90d8uyap9a'
+        // ]);
+
+        // SuratMasuk::create([
+        //     'id' => '2',
+        //     'user_id' => '2',
+        //     'nama_suratmasuk' => '879324 bapeda',
+        //     'tanggal_suratmasuk' => '31 januari',
+        //     'pengirim' => 'putra',
+        //     'catatan' => 'ea ea ea ea',
+        //     'file_suratmasuk' => 'vbnas90d8uyap9a'
+        // ]);
 
         Status::create([
             'id' => '1',
